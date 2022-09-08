7165209bcefdd250ec718905a2854aad64acd3e7
Chase Pattee <86315514+DataNerd2021@users.noreply.github.com>
2022-09-08 13:00:54 -0600

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -44,64 +44,74 @@ radio_btn = st.radio('Search by:', options=['Artist', 'Album', 'Track'], horizon
 # if the user chooses to search by Artist
 
 if radio_btn == 'Artist':
-    artist_search = st.text_input('Enter Search Here:', placeholder='Enter artist name...')
-    df_results = df[df['Artist Name'].str.startswith(artist_search.title(), na=False)]
-    select_item = st.multiselect('Select Rows', df_results['Artist Name'].index[:1000]) 
-    artist_results = st.dataframe(df_results[['Track Name', 'Album Name', 'Artist Name']])
-    st.write('#### Your Playlist:')
-    select_item = pd.DataFrame(select_item)
-    select_item = select_item.merge(df_results, how='left', left_on=0, right_on=df_results.index)
-    select_item['rank'] = select_item['rank'].astype('int64')
-    select_item = pd.DataFrame(select_item)
-    select_item = select_item[['Track Name', 'Album Name', 'Artist Name', 'Points']]
+    try:
+        artist_search = st.text_input('Enter Search Here:', placeholder='Enter artist name...')
+        df_results = df[df['Artist Name'].str.startswith(artist_search.title(), na=False)]
+        select_item = st.multiselect('Select Rows', df_results['Artist Name'][:1000].index) 
+        artist_results = st.dataframe(df_results[['Track Name', 'Album Name', 'Artist Name']])
+        st.write('#### Your Playlist:')
+        select_item = pd.DataFrame(select_item)
+        select_item = select_item.merge(df_results, how='left', left_on=0, right_on=df_results.index)
+        select_item['rank'] = select_item['rank'].astype('int64')
+        select_item = pd.DataFrame(select_item)
+        select_item = select_item[['Track Name', 'Album Name', 'Artist Name', 'Points']]
     
-    items_selected = st.dataframe(select_item)
-    total_points = select_item['Points'].sum()
-    col1, col2, col3 = st.columns([1, 1, 1])
-    with col1:
-        submit_btn = st.button('Submit')
-        if submit_btn:
-            with col2:
-                st.write(f'### Total Points: {total_points}')
+        items_selected = st.dataframe(select_item)
+        total_points = select_item['Points'].sum()
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
+            submit_btn = st.button('Submit')
+            if submit_btn:
+                with col2:
+                    st.write(f'### Total Points: {total_points}')
+    except(KeyError):
+        st.write('***Please select a Track before previewing playlist.***')
+        
 elif radio_btn == 'Album':
-    album_search = st.text_input('Enter Search Here:', placeholder='Enter album name...')
-    df_results = df[df['Album Name'].str.startswith(album_search.title(), na=False)]
-    select_item = st.multiselect('Select Rows', df_results['Album Name'].index[:1000]) 
-    artist_results = st.dataframe(df_results[['Track Name', 'Album Name', 'Artist Name']])
-    st.write('#### Your Playlist:')
-    select_item = pd.DataFrame(select_item)
-    select_item = select_item.merge(df_results, how='left', left_on=0, right_on=df_results.index)
-    select_item['rank'] = select_item['rank'].astype('int64')
-    select_item = pd.DataFrame(select_item)
-    select_item = select_item[['Track Name', 'Album Name', 'Artist Name', 'Points']]
+    try:
+        album_search = st.text_input('Enter Search Here:', placeholder='Enter album name...')
+        df_results = df[df['Album Name'].str.startswith(album_search.title(), na=False)]
+        select_item = st.multiselect('Select Rows', df_results['Album Name'].index[:1000]) 
+        artist_results = st.dataframe(df_results[['Track Name', 'Album Name', 'Artist Name']])
+        st.write('#### Your Playlist:')
+        select_item = pd.DataFrame(select_item)
+        select_item = select_item.merge(df_results, how='left', left_on=0, right_on=df_results.index)
+        select_item['rank'] = select_item['rank'].astype('int64')
+        select_item = pd.DataFrame(select_item)
+        select_item = select_item[['Track Name', 'Album Name', 'Artist Name', 'Points']]
     
-    items_selected = st.dataframe(select_item)
-    total_points = select_item['Points'].sum()
-    col1, col2, col3 = st.columns([1, 1, 1])
-    with col1:
-        submit_btn = st.button('Submit')
-        if submit_btn:
-            with col2:
-                st.write(f'### Total Points: {total_points}')
-if radio_btn == 'Track':
-    track_search = st.text_input('Enter Search Here:', placeholder='Enter track name...')
-    df_results = df[df['Track Name'].str.startswith(track_search.title(), na=False)]
-    select_item = st.multiselect('Select Rows', df_results['Track Name'].index[:1000]) 
-    artist_results = st.dataframe(df_results[['Track Name', 'Album Name', 'Artist Name']])
-    st.write('#### Your Playlist:')
-    select_item = pd.DataFrame(select_item)
-    select_item = select_item.merge(df_results, how='left', left_on=0, right_on=df_results.index)
-    select_item['rank'] = select_item['rank'].astype('int64')
-    select_item = pd.DataFrame(select_item)
-    select_item = select_item[['Track Name', 'Album Name', 'Artist Name', 'Points']]
+        items_selected = st.dataframe(select_item)
+        total_points = select_item['Points'].sum()
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
+            submit_btn = st.button('Submit')
+            if submit_btn:
+                with col2:
+                    st.write(f'### Total Points: {total_points}')
+    except(KeyError):
+        st.write('***Please select a Track before previewing playlist.***')
+elif radio_btn == 'Track':
+    try:
+        track_search = st.text_input('Enter Search Here:', placeholder='Enter track name...')
+        df_results = df[df['Track Name'].str.startswith(track_search.title(), na=False)]
+        select_item = st.multiselect('Select Rows', df_results['Artist Name'].index[:1000]) 
+        artist_results = st.dataframe(df_results[['Track Name', 'Album Name', 'Artist Name']])
+        st.write('#### Your Playlist:')
+        select_item = pd.DataFrame(select_item)
+        select_item = select_item.merge(df_results, how='left', left_on=0, right_on=df_results.index)
+        select_item['rank'] = select_item['rank'].astype('int64')
+        select_item = pd.DataFrame(select_item)
+        select_item = select_item[['Track Name', 'Album Name', 'Artist Name', 'Points']]
     
-    items_selected = st.dataframe(select_item)
-    total_points = select_item['Points'].sum()
-    col1, col2, col3 = st.columns([1, 1, 1])
-    with col1:
-        submit_btn = st.button('Submit')
-        if submit_btn:
-            with col2:
-                st.write(f'### Total Points: {total_points}')
+        items_selected = st.dataframe(select_item)
+        total_points = select_item['Points'].sum()
+        col1, col2, col3 = st.columns([1, 1, 1])
+        with col1:
+            submit_btn = st.button('Submit')
+            if submit_btn:
+                with col2:
+                    st.write(f'### Total Points: {total_points}')
+    except(KeyError):
+        st.write('***Please select a Track before previewing playlist.***')
     
 
